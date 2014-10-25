e127fd1fa9b3d616b2ad7838a67fc3ddeec86526
Dr Nic Williams <drnicwilliams@gmail.com>
2014-10-25 13:25:30 -0700

    main app looks for a manifest; else uses app name

--- main.go
+++ main.go
@@ -2,23 +2,50 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 
+	"github.com/cloudfoundry-community/cf-ssh/cfmanifest"
 	"github.com/codegangsta/cli"
+	"github.com/kr/pretty"
 )
 
 func cmdSSH(c *cli.Context) {
-	appName := c.Args().First()
-	if appName == "" {
-		fmt.Println("USAGE: cf-ssh APPNAME")
-		return
+	fmt.Println(c.String("manifest"))
+	manifestPath, err := filepath.Abs(c.String("manifest"))
+	if err != nil {
+		log.Fatal(err)
 	}
+	var manifest *cfmanifest.Manifest
+	if _, err := os.Stat(manifestPath); os.IsNotExist(err) {
+		appName := c.Args().First()
+		if appName == "" {
+			fmt.Println("USAGE: cf-ssh [APPNAME] [-f manifest.yml]")
+			return
+		}
 
+		manifest = cfmanifest.NewSSHManifest(appName)
+	} else {
+		manifest, err = cfmanifest.NewSSHManifestFromManifestPath(manifestPath)
+		if err != nil {
+			log.Fatalf("Manifest %s exists but failed to load: %s", manifestPath, err)
+		}
+	}
+	fmt.Printf("%# v\n", pretty.Formatter(*manifest))
 }
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "cf-ssh"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "manifest, f",
+			Value: "manifest.yml",
+			Usage: "Path to application manifest",
+		},
+	}
+
 	app.Usage = "SSH into a Cloud Foundry app container"
 	app.Action = cmdSSH
 
